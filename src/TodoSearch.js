ec504a4de6585ff3552937a804ec5d694743c8aa
melendez <melenava@gmail.com>
2024-09-17 20:29:23 -0600

    Agregar intelligense de React; Eventos de React

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,6 +1,11 @@
 function TodoSearch(){
     return(
-      <input placeholder="Buscar..." className="TodoSearch"></input>
+      <input placeholder="Buscar..." className="TodoSearch" onChange={(event) => {
+          console.log('TodoSearch');
+          console.log(event);
+          console.log(event.target);
+          console.log(event.target.value);
+      }}></input>
     );
   }
 
